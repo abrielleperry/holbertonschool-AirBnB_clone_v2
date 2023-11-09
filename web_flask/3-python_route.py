1a1108c11bbadda5037256f2aa3907abfba8b71a
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2023-11-09 13:27:15 -0600

    file 3 cp from 2

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""flask model"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """HBNB route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """HBNB route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """displays C with text input"""
+    text = text.replace("_", " ")
+    return 'C ' + text
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """displays C with text input"""
+    text = text.replace("_", " ")
+    return 'Python ' + text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
