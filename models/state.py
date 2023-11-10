2553ac47a9174baac08cbe984cd68e54e0f6730d
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2023-11-10 11:23:20 -0600

    rearranging lines

--- models/state.py
+++ models/state.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from os import getenv
 from models.city import City
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship(
+            "City", backref="state", cascade="all, delete")
+
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship(
-            "City", backref="state", cascade="all, delete"
-        )
-    else:
         @property
         def cities(self):
             """returns list of City instances upon state_id"""
